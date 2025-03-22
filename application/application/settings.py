ad5db9a255e89039128ec8b81589fcd4ca2c212d
ram-25082005 <rampalla2005@gmail.com>
2025-03-22 22:03:23 +0530

    ..

--- application/application/settings.py
+++ application/application/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+ALLOWED_ORIGINS=["https://portfolio-production-864d.up.railway.app"]
 
 
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"D:\\MACHINE LEARNING\\PROJECTS\\Book reccomendations\\application\\templates")],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
